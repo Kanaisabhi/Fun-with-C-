bb0aefa2e371636fcf2b541c7c810b5d9ba93864
Kanaisabhi <ks8757579@gmail.com>
2025-02-15 14:46:23 +0530

    Create Madlib Game.cpp

---
+++ Madlib Game.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+    string color;
+    string pluralNoun;
+    string Celebrity;
+    cout<<"enter color name: "<<endl;
+    cin>>color;
+    cout<<"enter plural noun: "<<endl;
+    cin>>pluralNoun;
+    cout<<"enter Celebrity name: "<<endl;
+    cin>>Celebrity;
+    cout<<"Roses are "<<color<<endl;
+    cout<<pluralNoun<<" are blue"<<endl;
+    cout<<"I love "<<Celebrity<<endl;
+}
